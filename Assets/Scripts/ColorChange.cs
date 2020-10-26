a8ee1ede39767d69395632588dddc8f3e9b5f34d
Chris Ravosa <christopher.ravosa1@marist.edu>
2020-10-26 14:54:00 -0400

    Adding everything i have

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class ColorChange : MonoBehaviour
+{
+    private GameObject volume;
+
+    private ColorAdjustments colorAdjustments;
+    private FilmGrain filmGrain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume = GameObject.Find("Global Volume");
+        contrast = volume.GetComponent<ColorAdjustments>().contrast;
+        filmGrain = volume.GetComponent<FilmGrain>();
+
+        colorAdjustments.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
