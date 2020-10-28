9f459fea5c5402108159e20eec5529fb4c37670c
Chris Ravosa <christopher.ravosa1@marist.edu>
2020-10-28 14:17:15 -0400

    Melissa is pee pee poo poo

---
+++ Assets/Scripts/AllLevels/ScrollScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+    public GameObject planeGameObject;
+    private Renderer planeRenderer;
+
+    float scrollSpeed = 0.1f;
+
+    void Start()
+    {
+        planeRenderer = planeGameObject.GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        Vector2 textureOffset = new Vector2(Time.time * scrollSpeed, 0);
+        planeRenderer.material.mainTextureOffset = textureOffset;
+    }
+}
---
+++ Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Make public variables to store the UI elements for the main menu.
+    public Button startButton;
+    public Button quitButton;
+
+    // This delay allows the screen to fade out before changing levels.
+    public float transitionDelay = 2f;
+
+    void Start()
+    {
+        // Add listeners to make buttons functional.
+        startButton.onClick.AddListener(EnableTransition);
+
+        quitButton.onClick.AddListener(QuitGame);
+    } // Start
+
+    void EnableTransition()
+    {
+        // Fade the screen out with this panel...
+        // transitionPanel.SetActive(true);
+
+        // ...and load the next scene in x seconds.
+        Invoke("LoadShipyard", transitionDelay);
+    } // EnableTransition
+
+    void LoadShipyard()
+    {
+        // Load "Setup" scene.
+        SceneManager.LoadScene("Shipyard");
+    } // LoadSetup
+
+    void QuitGame()
+    {
+        Application.Quit();
+    } // QuiteGame
+}
